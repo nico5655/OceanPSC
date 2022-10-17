ab0715805cf80c70d974084d7720b28b9714dd71
Edarfix <1edarfix1@gmail.com>
2022-10-17 17:37:52 +0200

    truc

--- OceanPSC/OceanPSC.py
+++ OceanPSC/OceanPSC.py
@@ -1,3 +1,31 @@
 
-def main():
-    pass
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans, MiniBatchKMeans
+ 
+# Génération d'un jeu de données aléatoire
+#modèle 0 : altitude moyenne, gradien moyen, amplitude d'altitude [moy,g,delta,x,y] puis les coord
+#Coordonnees = [x,y] sur sur la terre, comment on gère le baille sphérique ?
+#données : entre altitude max et min [m,M], quelle proportion entre [m,am+bM] ect [..,M]
+#données : distanc pôle ou cmt géréer la déformation de la projection ?
+# 
+data = np.random.rand(10,5) #si on veut des coeffitients dans les données il faut e faire avant -> rezize + coef car norme L2
+nb_cluster = 3
+
+model = MiniBatchKMeans(nb_cluster) #plus rapide 
+#Pour d'autres algo https://scikit-learn.org/stable/modules/clustering.html#k-means
+model.fit(data, y=None, sample_weight= None)
+
+model.labels_ #tab donnée i dans cluster model.labels_[j]
+
+
+#Les caractéristiques de nos types sont les coordonnées des centroides, il serait bien d'avoir des
+#distributions satistiques pour choisir un point qui ne soit pas exactement le centroide -> plus tard
+
+
+
+
+
+
+
+
