bacef666956ffe261c4ac8caed5343dff5f665c7
Nicolas Welti <nicolas.welti@free.fr>
2022-11-23 16:52:21 +0100

    vf

--- OceanPSC/Generation.py
+++ OceanPSC/Generation.py
@@ -79,8 +79,80 @@ def genere_bords(Carte, Tab_types, Tuile_a_generer, Liste_types):
             profil(l, h, t1, t2, (1, 0))
             profil(l, h, t1, t4, (0, 1))
 
-def genere_tuile(Carte, Tab_types, i, j, Tuile_a_generer, Liste_types):
-    pass
+def genere_tuile(Carte,Tab_types,i,j,Tuile_a_generer,Liste_types):
+    
+    indice = int(Tab_types[i][j])
+    altitude_moyenne = Liste_types[indice][0]
+    gradient_moyen = Liste_types[indice][1]
+    amplitude_altitude = Liste_types[indice][2]
+        
+
+    #Smooth Step function
+    def S(x):
+        return 3 * x * x - 2 * x * x * x
+
+    #Pseudo-random numbers
+    def a(i,j):
+        u,v = 50 * (i / np.pi - np.floor(i / np.pi)),50 * (j / np.pi - np.floor(j / np.pi))
+        return 2 * (u * v * (u + v) - np.floor(u * v * (u + v))) - 1
+
+    def am(i:int,j:int):
+        ###il faut ajouter une ligne pour agir sur les coefficients sur les
+        ###bords et sur des points à l'intérieur de la tuile
+        if i % taille_tuile == 0 or j % taille_tuile == 0 or Carte.data[int(i // taille_tuile)][int(j // taille_tuile)] >= 0:
+            return max(1,(Carte.data[i / taille_tuile,j / taille_tuile] - altitude_moyenne) / amplitude_altitude)
+        ###n'oublie pas les bords périodiques
+        u,v = 50 * (i / np.pi - np.floor(i / np.pi)),50 * (j / np.pi - np.floor(j / np.pi))
+        return 2 * (u * v * (u + v) - np.floor(u * v * (u + v))) - 1
+
+
+    #Conditions de raccordement
+    def b(i,j): return a(i + 1,j)
+    def c(i,j): return a(i,j + 1)
+    def d(i,j): return a(i + 1,j + 1)
+
+    def bm(i,j): return am(i + 1,j)
+    def cm(i,j): return am(i,j + 1)
+    def dm(i,j): return am(i + 1,j + 1)
+
+    #Noise local
+    def N(x,y):
+        i,j = np.floor(x),np.floor(y)
+        return a(i,j) + (b(i,j) - a(i,j)) * S(x - i) + (c(i,j) - a(i,j)) * S(y - j) + (a(i,j) - b(i,j) - c(i,j) + d(i,j)) * S(x - i) * S(y - j)
+
+    #Noise local modifié
+    def Nm(x,y):
+        i,j = np.floor(x),np.floor(y)
+        return am(i,j) + (bm(i,j) - am(i,j)) * S(x - i) + (cm(i,j) - am(i,j)) * S(y - j) + (am(i,j) - bm(i,j) - cm(i,j) + dm(i,j)) * S(x - i) * S(y - j)
+
+
+
+
+    temp = np.zeros((hauteur * taille_tuile,largeur * taille_tuile))
+    for dx in range(taille_tuile):
+        for dy in range(taille_tuile):
+            x = taille_tuile * i + dx
+            y = taille_tuile * j + dy
+            temp[x][y] = .5 * (amplitude_altitude * Nm(x / taille_tuile,y / taille_tuile) + altitude_moyenne)
+
+    #Génératrice du terrain
+    def f(x,y,iterations):
+        result = 0
+        p = 1
+        for i in range(1,iterations):
+            result+=N(p * x,p * y) / p
+            p*=2
+            temp = x
+            x = 3 / 5 * x - 4 / 5 * y
+            y = 4 / 5 * temp + 3 / 5 * y
+        return result
+
+    for dx in range(taille_tuile):
+        for dy in range(taille_tuile):
+            x = taille_tuile * i + dx
+            y = taille_tuile * j + dy
+            if Carte.data[x,y] == -np.inf:
+                Carte.data[x,y] = temp[x][y] + .5 * (amplitude_altitude * f(x / taille_tuile,y / taille_tuile,1) + altitude_moyenne)
 
 
 def show3d(map2):
